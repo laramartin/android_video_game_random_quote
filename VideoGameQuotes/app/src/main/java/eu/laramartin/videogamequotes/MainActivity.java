7427c1b322e35882ae845b786e3c0934cc078baf
Lara Martin <larikimc@gmail.com>
2016-05-31 13:58:01 +0200

    share button showed on actionbar

--- VideoGameQuotes/app/src/main/java/eu/laramartin/videogamequotes/MainActivity.java
+++ VideoGameQuotes/app/src/main/java/eu/laramartin/videogamequotes/MainActivity.java
@@ -1,18 +1,33 @@
 package eu.laramartin.videogamequotes;
 
+import android.annotation.TargetApi;
+import android.app.ActionBar;
+import android.content.Intent;
+import android.graphics.drawable.Icon;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-import java.text.NumberFormat;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton sharingButton = new ImageButton(this);
+        sharingButton.setLayoutParams(new ViewGroup.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT));
+        sharingButton.setImageIcon(Icon.createWithContentUri("@android:drawable/ic_menu_share"));
     }
 
 //    this method is called when "new quote" button is clicked
@@ -25,4 +40,29 @@ public class MainActivity extends AppCompatActivity {
         authorTextView.setText("-- King of All Cosmos");
         videoGameTextView.setText("\uD83C\uDFAE Katamari Damacy");
     }
+
+//    private ShareActionProvider mShareActionProvider;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate menu resource file.
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+//        // Locate MenuItem with ShareActionProvider
+//        MenuItem item = menu.findItem(R.id.menu_item_share);
+//
+//        // Fetch and store ShareActionProvider
+//        mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+
+        // Return true to display menu
+        return true;
+    }
+
+//    // Call to update the share intent
+//    private void setShareIntent(Intent shareIntent) {
+//        if (mShareActionProvider != null) {
+//            mShareActionProvider.setShareIntent(shareIntent);
+//        }
+//    }
 }
+
