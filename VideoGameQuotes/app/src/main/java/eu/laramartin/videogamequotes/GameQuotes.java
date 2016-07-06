f303f433cefe2b86608a10b89c6d3843e0cdb261
Lara Martin <larikimc@gmail.com>
2016-07-06 14:04:27 +0200

    new quotes added

--- VideoGameQuotes/app/src/main/java/eu/laramartin/videogamequotes/GameQuotes.java
+++ VideoGameQuotes/app/src/main/java/eu/laramartin/videogamequotes/GameQuotes.java
@@ -41,12 +41,15 @@ public class GameQuotes {
                 "Guild Wars 2"));
 
         list.add(new Quote(
-                "Working for a better tomorrow isn't just one-time thing. The tomorrows keep coming.",
+                "Working for a better tomorrow isn't just one-time thing. The " +
+                        "tomorrows keep coming.",
                 "Yu Narukami",
                 "Persona 4"));
 
         list.add(new Quote(
-                "Belive in yourself... If you don't have that... it doesn't matter how many talents you have, you still won't be able to hold your head up high...",
+                "Belive in yourself... If you don't have that... it doesn't matter " +
+                        "how many talents you have, you still won't be able to hold " +
+                        "your head up high...",
                 "Chiaki",
                 "Danganronpa 2"));
 
@@ -54,5 +57,83 @@ public class GameQuotes {
                 "All your base are belong to us",
                 "Cats",
                 "Zero Wing"));
+
+        list.add(new Quote(
+                "Its a-me, Mario!",
+                "Mario Auditore",
+                "Assassins Creed II"));
+
+        list.add(new Quote(
+                "Conglaturation! You have completed a great game!",
+                "End of the Game",
+                "Ghostbuster"));
+
+        list.add(new Quote(
+                "A winner is you",
+                "End of the Game",
+                "Pro Wrestling"));
+
+        list.add(new Quote(
+                "At the end of the experiment, you will be baked and then there will be cake.",
+                "GLaDOS",
+                "Portal"));
+
+        list.add(new Quote(
+                "Oh, hi. So, how are you holding up? BECAUSE I'M A POTATO!",
+                "GLaDOS",
+                "Portal 2"));
+
+        list.add(new Quote(
+                "The right man in the wrong place can make all the difference in the world.",
+                "G-Man",
+                "Half-Life 2"));
+
+        list.add(new Quote(
+                "Is a man not entitled to the sweat of his brow?",
+                "Andrew Ryan",
+                "BioShock"));
+
+        list.add(new Quote(
+                "Wakka wakka wakka!",
+                "Pac-Man",
+                "Pac-Man"));
+
+        list.add(new Quote(
+                "Sul sul",
+                "Sim",
+                "Sims"));
+
+        list.add(new Quote(
+                "BECAUSE I'M CRAAAAAAAZY!",
+                "Crazy Dave",
+                "Plants vs. Zombies"));
+
+        list.add(new Quote(
+                "I hate tombs.",
+                "Lara Croft",
+                "Tomb Raider"));
+
+        list.add(new Quote(
+                "If you ask me, it's a great time to be alive. There's not even that much " +
+                        "disease considering we never wash!",
+                "Random villager",
+                "Fable II"));
+
+        list.add(new Quote(
+                "Uka Uka is free?! No, it cannot be! Evil, great evil has come!",
+                "Aku Aku",
+                "Crash Bandicoot 3: Warped"));
+
+        list.add(new Quote(
+                "A choice from the gods is as useless as the gods themselves.",
+                "Kratos",
+                "God of War II"));
+
+        list.add(new Quote(
+                "It's dangerous to go alone! Take this.",
+                "Old Man",
+                "Legend of Zelda"));
+
+
     }
 }
--- VideoGameQuotes/app/src/main/java/eu/laramartin/videogamequotes/MainActivity.java
+++ VideoGameQuotes/app/src/main/java/eu/laramartin/videogamequotes/MainActivity.java
@@ -2,21 +2,16 @@ package eu.laramartin.videogamequotes;
 
 import android.app.ActionBar;
 import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
-
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -26,83 +21,13 @@ public class MainActivity extends AppCompatActivity {
 
     int lastRandomNum = 0;
 
-//    Quote katamari = new Quote(
-//            "We are moved to tears by the size of this thing.",
-//            "King of All Cosmos",
-//            "Katamari Damacy");
-//
-//    Quote lastOfUs = new Quote(
-//            "Endure and survive.",
-//            "Ellie",
-//            "The Last of Us");
-//
-//    Quote borderlands2 = new Quote(
-//            "Nothing is more badass than treating a woman with respect.",
-//            "Mr. Torgue",
-//            "Borderlands 2"
-//            );
-//
-//    Quote mgs = new Quote(
-//            "It's easy to forget what a sin is in the middle of a battlefield.",
-//            "Solid Snake",
-//            "Metal Gear Solid"
-//            );
-//
-//    Quote marioBross = new Quote(
-//            "Thank you Mario! But our Princess is in another castle!",
-//            "Toad",
-//            "Super Mario Bros."
-//            );
-//
-//    Quote guildWars2 = new Quote(
-//            "Knowledge is useless if it is not used.",
-//            "Gixx",
-//            "Guild Wars 2"
-//            );
-//
-//    Quote persona4 = new Quote(
-//            "Working for a better tomorrow isn't just one-time thing. The tomorrows keep coming.",
-//            "Yu Narukami",
-//            "Persona 4"
-//            );
-//
-//    Quote danganronpa2 = new Quote(
-//            "Belive in yourself... If you don't have that... it doesn't matter how many talents you have, you still won't be able to hold your head up high...",
-//            "Chiaki",
-//            "Danganronpa 2"
-//            );
-//
-//    Quote zeroWing = new Quote(
-//            "All your base are belong to us",
-//            "Cats",
-//            "Zero Wing"
-//            );
-
     List<Quote> listOfQuotes = new ArrayList<>();
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
-    GameQuotes.initQuotes(listOfQuotes);
 
-//    public void fillArray(){
-//        listOfQuotes.add(katamari);
-//        listOfQuotes.add(lastOfUs);
-//        listOfQuotes.add(borderlands2);
-//        listOfQuotes.add(mgs);
-//        listOfQuotes.add(marioBross);
-//        listOfQuotes.add(guildWars2);
-//        listOfQuotes.add(persona4);
-//        listOfQuotes.add(danganronpa2);
-//        listOfQuotes.add(zeroWing);
-//    }
-
-    private int randomNumber() {
+    private int randomNumber(){
         Random r = new Random();
         int length = listOfQuotes.size();
         int num = r.nextInt(length - 1) + 1;
-        if (num != lastRandomNum) {
+        if (num != lastRandomNum){
             lastRandomNum = num;
             return num;
         } else {
@@ -110,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private Quote getQuote() {
+    private Quote getQuote(){
         Quote actualQuote = listOfQuotes.get(randomNumber());
         return actualQuote;
     }
@@ -123,10 +48,9 @@ public class MainActivity extends AppCompatActivity {
         sharingButton.setLayoutParams(new ViewGroup.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT));
         sharingButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_share_black_24dp));
         sharingButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_info_white_24dp));
-//        fillArray();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        GameQuotes.initQuotes(listOfQuotes);
+        Log.v("mainactivity", "quotes list: " + listOfQuotes.size());
     }
 
     public void newQuote(View view) {
@@ -166,60 +90,19 @@ public class MainActivity extends AppCompatActivity {
         String shareBody = "\"" + actualQuote.phrase + "\"" +
                 "\n-- " + actualQuote.author +
                 "\n" + "\uD83C\uDFAE " + actualQuote.game;
-        Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
         sharingIntent.setType("text/plain");
-        sharingIntent.putExtra(Intent.EXTRA_SUBJECT, "Game Quote");
-        sharingIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Game Quote");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
         startActivity(Intent.createChooser(sharingIntent, "share with"));
     }
 
-    private void infoAboutMe() {
+    private void infoAboutMe(){
         new AlertDialog.Builder(this)
                 .setTitle("About Me")
                 .setMessage("This application has been developed by Lara Martin.")
                 .setIcon(R.drawable.ic_error_black_24dp)
                 .show();
-
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://eu.laramartin.videogamequotes/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://eu.laramartin.videogamequotes/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
     }
 }
 
