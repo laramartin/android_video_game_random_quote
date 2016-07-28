bfc3bd232d324705ea108d3af59307034ca4b0f4
Lara Martin <larikimc@gmail.com>
2016-07-28 14:24:34 +0200

    added quotes

--- VideoGameQuotes/app/src/main/java/eu/laramartin/videogamequotes/GameQuotes.java
+++ VideoGameQuotes/app/src/main/java/eu/laramartin/videogamequotes/GameQuotes.java
@@ -15,7 +15,6 @@ public class GameQuotes {
                 "Katamari Damacy"));
 
         list.add(new Quote(
-
                 "Endure and survive.",
                 "Ellie",
                 "The Last of Us"));
@@ -59,9 +58,9 @@ public class GameQuotes {
                 "Zero Wing"));
 
         list.add(new Quote(
-                "Its a-me, Mario!",
-                "Mario Auditore",
-                "Assassins Creed II"));
+                "It's me, Mario",
+                "Mario",
+                "Super Mario 64"));
 
         list.add(new Quote(
                 "Conglaturation! You have completed a great game!",
@@ -132,8 +131,140 @@ public class GameQuotes {
         list.add(new Quote(
                 "It's dangerous to go alone! Take this.",
                 "Old Man",
-                "Legend of Zelda"));
+                "The Legend of Zelda"));
 
+        list.add(new Quote(
+                "It's in our nature to want to forget truths that keep us awake at night.",
+                "Ignacio Rivas",
+                "Fallout New Vegas"));
 
+        list.add(new Quote(
+                "If you want to avoid battles, stay out of the grassy areas!",
+                "Signpost in Viridian Forest",
+                "Pokémon"));
+
+        list.add(new Quote(
+                "We all make choices, but in the end, our choices make us.",
+                "Andrew Ryan",
+                "BioShock"));
+
+        list.add(new Quote(
+                "A hero need not speak. When he is gone, the world will speak for him.",
+                "Master Chief John-117",
+                "Halo"));
+
+        list.add(new Quote(
+                "Tell my wife, I had another wife.",
+                "Dying Secret Serviceman",
+                "Saints Row IV"));
+
+        list.add(new Quote(
+                "What is better - to be born good, or to overcome your evil nature through " +
+                        "great effort?",
+                "Paarthurnax",
+                "Skyrim"));
+
+        list.add(new Quote(
+                "Yo! Champ in making!",
+                "Giovanni, Gym Leader",
+                "Pokémon"));
+
+        list.add(new Quote(
+                "Love is just a chemical. We give it meaning by choice.",
+                "Eleanor Lamb",
+                "BioShock 2"));
+
+        list.add(new Quote(
+                "Wanting something doesn’t give you the right to have it.",
+                "Ezio Auditore",
+                "Assassins Creed II"));
+
+        list.add(new Quote(
+                "To like is to want, but love is to give.",
+                "Waitress",
+                "Catherine"));
+
+        list.add(new Quote(
+                "In the darkest hour, there is always a way out.",
+                "Eve",
+                "Mass Effect 3"));
+
+        list.add(new Quote(
+                "You look familiar. Were you ever on a milk carton?",
+                "Malkavian",
+                "Vampire: The Masquerade - Bloodlines"));
+
+        list.add(new Quote(
+                "Don't wish it were easier, wish you were better.",
+                "Chief",
+                "Animal Crossing"));
+
+        list.add(new Quote(
+                "War is where the young and stupid are tricked by the old and bitter " +
+                        "into killing each other.",
+                "Niko Bellic",
+                "GTA IV"));
+
+        list.add(new Quote(
+                "And remember, the universe has a wonderful sense of humor. The trick is " +
+                        "learning how to take a joke.",
+                "Orvus",
+                "Ratchet & Clank"));
+
+        list.add(new Quote(
+                "You either hang on to your morals and die, or﻿ do whatever it takes to survive.",
+                "Ellie",
+                "The Last of Us"));
+
+        list.add(new Quote(
+                "I'm the conductor of the poop train!",
+                "Krieg",
+                "Borderlands 2"));
+
+        list.add(new Quote(
+                "Legendary!",
+                "Announcer",
+                "League of Legends"));
+
+        list.add(new Quote(
+                "I don't need a weapon; my friends are my power!",
+                "Sora",
+                "Kingdom Hearts"));
+
+        list.add(new Quote(
+                "What is a man? A miserable pile of secrets. but enough talk...have at you!",
+                "Dracula",
+                "Castlevania: Symphony of the Night"));
+
+        list.add(new Quote(
+                "Today's been a bad day, and it's put me in a real shitty mood. It's just your " +
+                        "bad luck... to have run into me.",
+                "Kazuma Kiryu",
+                "Yakuza 1"));
+
+        list.add(new Quote(
+                "Snake? Snake!? Snaaaake!",
+                "Kaz",
+                "MGS"));
+
+        list.add(new Quote(
+                "Sorry, I'm Dead.",
+                "Boss #2",
+                "Monster Party"));
+
+        list.add(new Quote(
+                "Buy somethin' will ya!",
+                "Shopkeeper",
+                "The Legend of Zelda"));
+
+        list.add(new Quote(
+                "Snake? SNAAAAAAAAKE!!!",
+                "Colonel Campbell",
+                "Metal Gear Solid"));
+
+        list.add(new Quote(
+                "It's time to kick ass and chew bubble gum...and I'm all out of gum.",
+                "Duke Nukem",
+                "Duke Nukem 3D"));
     }
 }
